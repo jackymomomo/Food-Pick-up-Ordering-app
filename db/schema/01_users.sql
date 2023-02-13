6351bda6105be62b0015079f706ef88c20e3fddb
Abe <abrahamrodgers@gmail.com>
2023-02-13 12:17:05 -0800

    add files so can pull

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,34 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+-- DROP TABLE IF EXISTS users CASCADE;
+-- I understand what this statement does but am not clear why it's used
+
+
+
+CREATE TABLE menu_item (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  item_name VARCHAR(255) NOT NULL,
+  price INT NOT NULL,
+  -- the price is listed in cents and not with a decimal
+  appetizer BOOLEAN NOT NULL,
+  main BOOLEAN NOT NULL,
+  soup BOOLEAN NOT NULL
+);
+
+CREATE TABLE order_menu_item (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  order_id INT NOT NULL,
+  menu_item_id INT NOT NULL,
+  qty_ordered INT NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES food_order(id),
+  FOREIGN KEY (menu_item_id) REFERENCES menu_item(id)
+);
+
+CREATE TABLE food_order (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT NOT NULL,
+  customer_phone VARCHAR(255) NOT NULL,
+  order_status ENUM('PENDING', 'CONFIRMED', 'CANCELLED') NOT NULL,
+  order_date_time DATETIME NOT NULL,
+  total_amount INT NOT NULL
 );
---
+++ db/seeds/menu_items.sql
@@ -0,0 +1,71 @@
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Spring Rolls", 800, 1, 0, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Steamed Dumplings", 750, 1, 0, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Fried Wontons", 700, 1, 0, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Scallion Pancakes", 675, 1, 0, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Spicy Sesame Noodles", 850, 1, 0, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Wonton Soup", 750, 0, 0, 1);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Hot and Sour Soup", 700, 0, 0, 1);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Egg Drop Soup", 750, 0, 0, 1);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Kung Pao Chicken", 1400, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Moo Shu Pork", 1400, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Sweet and Sour Pork", 1350, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Beef and Broccoli", 1350, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Shrimp with Lobster Sauce", 1700, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Mongolian Beef", 1350, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Sesame Tofu", 1200, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Vegetable Fried Rice", 1300, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Chicken with Cashews", 1500, 0, 1, 0);
+
+INSERT INTO menu_item (item_name, price, appetizer, main, soup)
+VALUES
+("Mapo Tofu", 1200, 0, 1, 0);

60ecddc24da11eb4907c7b780491968bf8605ad1
jackymomomo <jacklloydrobinson@gmail.com>
2023-02-14 13:40:03 -0800

    ability to click an image and be given a drop down menu added, addedd images folder ability to click again on image to stop displaying menu, added menu

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,17 @@
 // Client facing scripts here
+const foodItems = document.querySelectorAll('.food-item');
+const menus = document.querySelectorAll('.menu');
+
+foodItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    if (menus[index].style.display === 'none') {
+      menus.forEach((menu) => {
+        menu.style.display = 'none';
+      });
+      menus[index].style.display = 'block';
+    } else {
+      menus[index].style.display = 'none';
+    }
+  });
+});
+
